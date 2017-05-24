bdf69dabb7e7635a1796d7642a331f3f73c25fa0
David Wu <dawu@pivotal.io>
2017-05-24 16:05:49 -0500

    Update of CF-Helper to make it more robust. In addition, added support to create connection strings

--- Core/System/Environment/EnvironmentVariables_JSON.NET/CFHelper.cs
+++ Core/System/Environment/EnvironmentVariables_JSON.NET/CFHelper.cs
@@ -12,14 +12,14 @@ namespace CFHelper
     {
         private IConfigurationRoot _configuration { get; set; }
         public dynamic vcap_services_data { get; set; }
-        public dynamic vcap_application_data {get; set; }
+        public dynamic vcap_application_data { get; set; }
 
         public CFEnvironmentVariables(IConfigurationRoot configuration)
         {
             _configuration = configuration;
 
-            var raw_vcap_app = _configuration["VCAP_APPLICATION"];
-            var raw_vcap_services = _configuration["VCAP_SERVICES"];
+            string raw_vcap_app = _configuration["VCAP_APPLICATION"];
+            string raw_vcap_services = _configuration["VCAP_SERVICES"];
 
             // If there's a vcap services entry, parse to a dynamic JObject;
             if (raw_vcap_services != null)
@@ -35,36 +35,97 @@ namespace CFHelper
             // Try to access the user-provided service.
             // Unfortunately, we can't do the dot notation here, since user-provided would be
             // an invalid property name.
-            var upsArray = vcap_services_data["user-provided"];
-            
-            if (upsArray != null)
+            if (Object.ReferenceEquals(null, vcap_services_data) == false)
             {
-                foreach(var ups in upsArray)
-                    if(ups.name == serviceName)
-                        return ups;
+                var upsArray = vcap_services_data["user-provided"];
+
+                if (upsArray != null && upsArray.HasValues)
+                {
+                    foreach (var ups in upsArray)
+                        if (ups.name == serviceName)
+                            return ups;
+                }
             }
-            
+
             return null;
         }
 
         public dynamic getInfoForService(string serviceTypeName, string serviceInstanceName = "")
         {
-            var serviceArray = vcap_services_data[serviceTypeName];
 
-            if (serviceArray != null)
+            if (Object.ReferenceEquals(null, vcap_services_data) == false)
             {
-                // If serviceInstanceName is empty, just return the first element of our services info array
-                if (serviceInstanceName == "")
-                {
-                    return serviceArray[0];
-                }
+                dynamic serviceArray = vcap_services_data[serviceTypeName];
 
-                foreach(var service in serviceArray)
-                    if(service.name == serviceInstanceName)
-                        return service;
+                if (serviceArray != null && serviceArray.HasValues)
+                {
+                    // If serviceInstanceName is empty, just return the first element of our services info array
+                    if (serviceInstanceName == "")
+                    {
+                        return serviceArray[0];
+                    }
+
+                    foreach (var service in serviceArray)
+                        if (service.name == serviceInstanceName)
+                            return service;
+                }
             }
 
             return null;
         }
+
+        public string getConnectionStringForDbService(string serviceTypeName, string serviceInstanceName = "", IDbConnectionStringFormatter formatter = null)
+        {
+            string connectionString = "";
+
+            if (Object.ReferenceEquals(null, vcap_services_data) == false)
+            {
+                dynamic serviceInfo = getInfoForService(serviceTypeName, serviceInstanceName);
+
+                if (Object.ReferenceEquals(null, serviceInfo) == false)
+                {
+                    // Default to use a Basic MS SQL Server connection string, if our formatter was not specified.
+                    if (formatter == null)
+                        formatter = new BasicSQLServerConnectionStringFormatter();
+
+                    var host = Convert.ToString(serviceInfo.credentials.hostname);
+                    var username = Convert.ToString(serviceInfo.credentials.username);
+                    var password = Convert.ToString(serviceInfo.credentials.password);
+                    var port = Convert.ToString(serviceInfo.credentials.port);
+                    var databaseName = Convert.ToString(serviceInfo.credentials.name);
+
+                    connectionString = formatter.Format(host, username, password, databaseName, port);
+                }
+            }
+            return connectionString;
+        }
     }
+
+    // Connection String Formatting helper classes
+    public interface IDbConnectionStringFormatter
+    {
+        string Format(string host, string username, string password, string databaseName, string port = null);
+    }
+
+    public class BasicMySQLConnectionStringFormatter : IDbConnectionStringFormatter
+	{
+        public string Format(string host, string username, string password, string databaseName, string port = null)
+        {
+            string connectionString = $"Server={host};Database={databaseName};Uid={username};Pwd={password};";
+
+            if (port != null)
+                connectionString += $"Port={port};";
+            
+
+            return connectionString;
+		}
+	}
+
+	public class BasicSQLServerConnectionStringFormatter : IDbConnectionStringFormatter
+	{
+		public string Format(string host, string username, string password, string databaseName, string port = null)
+		{
+			return $"Server={host};Database={databaseName};User ID={username};Password={password};";
+		}
+	}
 }
